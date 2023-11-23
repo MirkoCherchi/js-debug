84a64b2ca647bb7779e9478d92a9466b39152d60
MirkoCherchi <mirkocherchi1992@gmail.com>
2023-11-23 16:32:53 +0100

    feat: bundle-03

---
+++ bundle-03/main.js
@@ -0,0 +1,164 @@
+/*******************************************************************************
+​
+    Per questo esercizio, rispondi a queste domande:
+    1- Che cosa fa questo codice?
+    2- Sono presenti errori di sintassi?
+    3- Sono presenti errori logici?
+​
+    Inserisci i tuoi commenti con le risposte alla fine dell'esercizio
+​
+*******************************************************************************/
+
+
+// ESERCIZIO 1 (suggerimento: ci sono 6 errori)
+// const cars = [
+//     {
+//         manufacturer: 'Ford',
+//         model: 'Fiesta',
+//         type: 'diesel'
+//     },
+//     {
+//         manufacturer: 'Audi',
+//         model: 'A1',
+//         type: 'benzina'
+//     },
+//     {
+//         manufacturer: 'Volkswagen',
+//         model: 'Golf',
+//         type: 'Benzina'
+//     },
+//     {
+//         manufacturer: 'Fiat',
+//         model: 'Panda',
+//         type: 'metano'
+//     },
+//     {
+//         manufacturer: 'Fiat',
+//         model: 'Multipla',
+//         type: 'GPL'
+//     },
+//     {
+//         manufacturer: 'Tesla',
+//         model: 'Model 3',
+//         type: 'elettrico'
+//     },
+//     {
+//         manufacturer: 'Volkswagen',
+//         model: 'Polo',
+//         type: 'benzina'
+//     },
+//     {
+//         manufacturer: 'Ford',
+//         model: 'Kuga',
+//         type: 'Diesel'
+//     },
+//     {
+//         manufacturer: 'Seat',
+//         model: 'Ibiza',
+//         type: 'metano'
+//     }
+//     {
+//         manufacturer: 'Audi',
+//         model: 'R8',
+//         type: 'Benzina'
+//     },
+// ];
+
+// const gasolineCars = cars.filter( (auto) >= auto.type === 'benzina');
+
+// const dieselCars = cars.filter( (auto) => {
+//     auto.type === 'diesel';
+// });
+
+// const otherCars = cars.filter( (auto) => {
+//     return auto.type !== 'benzina' || auto.type !== 'diesel';
+// });
+
+// console.log('Auto a benzina');
+// console.log('*******************************');
+// console.log(gasolineCars);
+
+// console.log('Auto a diesel');
+// console.log('*******************************');
+// console.log(dieselCars);
+
+// console.log('Tutte le altre auto');
+// console.log(otherCars);
+
+
+//Esercizio 1: In questo codice abbiamo un array di oggetti. La prima cosa che facciamo è filtrare questi oggetti in gasolineCars, dieselCars, otherCars. Stampiamo in console il risultato dei filtri divisi per type. In questo codice ci sono 6 erorri:
+
+const cars = [
+    {
+        manufacturer: 'Ford',
+        model: 'Fiesta',
+        type: 'diesel'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'A1',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Golf',
+        type: 'Benzina'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Panda',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Multipla',
+        type: 'GPL'
+    },
+    {
+        manufacturer: 'Tesla',
+        model: 'Model 3',
+        type: 'elettrico'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Polo',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Ford',
+        model: 'Kuga',
+        type: 'Diesel'
+    },
+    {
+        manufacturer: 'Seat',
+        model: 'Ibiza',
+        type: 'metano'
+    },  //qui mancava una virgola
+    { 
+        manufacturer: 'Audi',
+        model: 'R8',
+        type: 'Benzina'
+    },
+];
+
+const gasolineCars = cars.filter((auto) => auto.type.toLowerCase() === 'benzina'); // qui inizialmente stavamo filtrando le auto con type benzina, ma dovremo aggiungere un .toLowerCase per essere sicuri che se nella scrittura dell'oggetto sia stato scritto il type con maiuscolo come in questo caso, ce lo trasforma in minuscolo.
+
+const dieselCars = cars.filter((auto) => auto.type.toLowerCase() === 'diesel'); // qui la stessa cosa dell'errore di prima sobbiamo aggiungere toLowerCase, inoltre eliminiamo il ';' dopo il 'diesel', togliamo anche le parentesi graffe perchè siamo su una sola riga e non useremo il return
+
+
+const otherCars = cars.filter((auto) => {
+    return auto.type.toLowerCase() !== 'benzina' && auto.type.toLowerCase() !== 'diesel'}); // qui abbiamo due errori, il primo errore è sempre l'errore del toLowerCase da aggiungere, il secondop errore dobbiamo inserire l'and ovvero && invece dell'or ||, anche qui inoltre eliminiamo il ';' dopo il 'diesel'
+
+
+console.log('Auto a benzina');
+console.log('*******************************');
+console.log(gasolineCars);
+
+console.log('Auto a diesel');
+console.log('*******************************');
+console.log(dieselCars);
+
+console.log('Tutte le altre auto');
+console.log(otherCars);
+
+
